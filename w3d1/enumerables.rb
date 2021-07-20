27bdd051aec81b696b20abb4a04b53b5f899d98e
arwen <arwenkim@Arwens-MacBook-Pro.local>
2021-07-19 17:26:43 -0700

    what we worked

--- w3d1/enumerables.rb
+++ w3d1/enumerables.rb
@@ -2,10 +2,14 @@ class Array
 
     def my_each(&prc)
         
-        new_arr = self.map do |el|
-            prc.call(el)
+        i = 0
+
+        while i < self.length
+            prc.call(self[i])
+            i += 1
         end
-        new_arr
+
+        self
     end
 
     def my_select(&prc)
@@ -14,6 +18,31 @@ class Array
             new_arr << el if prc.call(el)
         end
         new_arr
+    end
+
+    def my_reject(&prc)
+        self.select { |el| !prc.call(el) }
+    end
+
+    def my_any?(&prc)
+        self.any? { |el| prc.call(el)}
+    end
+
+    def my_all?(&prc)
+        self.all? { |el|  prc.call(el)}
+    end
+
+    def my_flatten
+        
+        flatten = []
+        self.my_each do |el|
+            if !el.is_a?(Array)
+                flatten << el
+            else
+                flatten += el.my_flatten
+            end
+        end
+        flatten
     end
 
 end
