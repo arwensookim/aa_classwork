9c4913aafa0181939d46ccce452b1a9c5ec39ab9
josephafern <60451011+josephafern@users.noreply.github.com>
2021-07-20 14:12:49 -0700

    created Board and Card class. Initialized board. Started Board#populate.

---
+++ w3d2/memory_puzzle/board.rb
@@ -0,0 +1,12 @@
+class Board
+    def initialize
+        @board = Array.new(4) { Array.new(4, '_') }
+    end
+
+    def populate
+        cards = ['K', 'Q', 'J', 'A', '10', '9', '8', '7']
+        (0...@board.length).each do |i|
+            
+        end
+    end
+end
---
+++ w3d2/memory_puzzle/card.rb
@@ -0,0 +1,10 @@
+class Card
+  def hide
+    face_up = false
+    
+  end
+
+  def reveal
+    
+  end
+end
