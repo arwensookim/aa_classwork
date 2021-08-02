14ca5606fd9c540ef1690b589780f77fe93832cf
arwen <arwenkim@Arwens-MacBook-Pro.local>
2021-08-02 15:40:03 -0700

    coded hash_set

--- w5d1/skeleton/lib/p03_hash_set.rb
+++ w5d1/skeleton/lib/p03_hash_set.rb
@@ -7,18 +7,29 @@ class HashSet
   end
 
   def insert(key)
+    return nil if include?(key)
+    if count >= @store.length
+      resize!
+    end
+    self[key] << key
+    @count += 1
   end
 
   def include?(key)
+    self[key].include?(key)
   end
 
   def remove(key)
+    return nil unless include?(key)
+    self[key].delete(key)
+    @count -= 1
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    return @store[num.hash % @store.length]
   end
 
   def num_buckets
@@ -26,5 +37,13 @@ class HashSet
   end
 
   def resize!
+    @count = 0
+    old_store = @store
+    @store = Array.new(old_store.length * 2) {Array.new}
+    old_store.each do |bucket|
+      bucket.each do |val|
+        insert(val)
+      end
+    end
   end
 end
