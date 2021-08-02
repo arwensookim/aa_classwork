4c3ca999cc2399db82ac1d9d663adb945a5f5a2b
Mauricio <mjlomeli@uci.edu>
2021-08-02 15:31:00 -0700

    finished hashing

--- w5d1/skeleton/lib/p02_hashing.rb
+++ w5d1/skeleton/lib/p02_hashing.rb
@@ -4,18 +4,33 @@ end
 
 class Array
   def hash
+    # [1, 2, 3].hash == [3, 2, 1].hash # => false
+    hash = 0
+    self.each_with_index do |el, i|
+      hash = hash ^ (el.hash & i.hash)
+    end
+    hash
   end
 end
 
 class String
   def hash
+    hash = 0
+    self.each_char.with_index do |c, i|
+      hash = hash ^ (c.ord.hash & i.hash)
+    end
+    hash
   end
 end
 
 class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
-  def hash
-    0
+  def Hash
+    hash = 0
+    self.each do |k, v|
+      hash = hash ^ (k.hash & v.hash)
+    end
+    hash
   end
 end
