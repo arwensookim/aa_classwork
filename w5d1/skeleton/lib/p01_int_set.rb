a9d66e5718eac5fc744ea9bdcebe4f3b637506f6
arwen <arwenkim@Arwens-MacBook-Pro.local>
2021-08-02 14:46:19 -0700

    finished p01_int_set.rb

--- w5d1/skeleton/lib/p01_int_set.rb
+++ w5d1/skeleton/lib/p01_int_set.rb
@@ -68,18 +68,29 @@ class ResizingIntSet
   end
 
   def insert(num)
+    return nil if include?(num)
+    if count >= @store.length
+      resize!
+    end
+    self[num] << num
+    @count += 1
   end
 
   def remove(num)
+    return nil unless include?(num)
+    self[num].delete(num)
+    @count -= 1
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    return @store[num % @store.length]
   end
 
   def num_buckets
@@ -87,5 +98,13 @@ class ResizingIntSet
   end
 
   def resize!
+    @count = 0
+    old_store = @store
+    @store = Array.new(old_store.length * 2) {Array.new}
+    old_store.each do |bucket|
+      bucket.each do |val|
+        insert(val)
+      end
+    end
   end
 end
