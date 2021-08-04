9c08cff777ad009591d02d3359315e08a34bf0b5
Alex Crooks <alexcrooks2@gmail.com>
2021-08-04 15:41:56 -0700

    Started classes methods

--- w5d3/aa_project/questions.rb
+++ w5d3/aa_project/questions.rb
@@ -32,7 +32,7 @@ class User
         if user.length < 1
             return nil
         else
-            User.new(user.first) # user? user.first?
+            User.new(user.first)
         end
     end
 
@@ -42,18 +42,133 @@ class User
         @lname = new_option['lname']
     end
 
-    
+    def self.find_by_name(fname, lname)
+        user = QuestionsDBConnection.instance.execute(<<-SQL, fname, lname)
+        SELECT
+            *
+        FROM
+            users
+        WHERE
+            fname = ? AND lname = ?
+        SQL
+        if user.length < 1
+            return nil
+        else
+            User.new(user.first)
+        end
+    end
+
+    def authored_questions
+        Questions.find_by_author_id(id)
+    end
+
+    def authored_replies
+        Reply.find_by_user_id(id)
+    end
+
+    def insert
+        raise "#{self} is already in the database" if self.id
+        QuestionsDBConnection.instance.execute(<<-SQL, self.fname, self.lname)
+            INSERT INTO
+                users(fname, lname)
+            VALUES
+                (?,?,?)
+        SQL
+        id = QuestionsDBConnection.instance.last_insert_row_id
+    end
+
+    def update
+        raise "#{self} not in database" unless self.id
+        QuestionsDBConnection.instance.execute(<<-SQL, self.fname, self.lname)
+      UPDATE
+        users
+      SET
+        fname = ?, lname = ?
+      WHERE
+        id = ?
+    SQL
+    end
 end
 
-# class Question
-#     def self.find_by_id
-#     end
-# end
+class Question
+    attr_accessor :id, :title, :body, :author_id
+    def self.all
+        questions = QuestionsDBConnection.instance.execute("SELECT * FROM questions")
+        questions.map { |question| Question.new(question)}
+    end
 
-# class QuestionFollow
-#     def self.find_by_id
-#     end
-# end
+
+    def self.find_by_id(id)
+        question = QuestionsDBConnection.instance.execute(<<-SQL, id)
+        SELECT
+            *
+        FROM
+            questions
+        WHERE 
+            id = ?
+        SQL
+        if question.length < 1
+            return nil
+        else
+            Question.new(question.first)
+        end
+    end
+
+    def initialize(new_option)
+        @id = new_option['id']
+        @title = new_option['title']
+        @body = new_option['body']
+        @author_id = new_option['author_id']
+    end
+
+    def self.find_by_author_id(author_id)
+        author = QuestionsDBConnection.instance.execute(<<-SQL, author_id)
+        SELECT
+            *
+        FROM
+            questions
+        WHERE 
+            author_id = ?
+        SQL
+        author.map {|author_question| Question.new(author_question)}
+    end
+
+    def insert
+        raise "#{self} is already in the database" if self.id
+        QuestionsDBConnection.instance.execute(<<-SQL, self.title, self.body, self.author_id)
+            INSERT INTO
+                questions(title, body, author_id)
+            VALUES
+                (?,?,?)
+        SQL
+        id = QuestionsDBConnection.instance.last_insert_row_id
+    end
+
+    def update
+        raise "#{self} not in database" unless self.id
+        QuestionsDBConnection.instance.execute(<<-SQL, self.title, self.body, self.author_id)
+      UPDATE
+        questions
+      SET
+        title = ?, body = ?, author_id = ?
+      WHERE
+        id = ?
+        SQL
+    end
+
+    def author
+        User.find_by_id(author_id)
+    end
+
+    def replies
+        Reply.find_by_question_id
+    end
+end
+
+class QuestionFollow
+    def self.find_by_id
+    end
+end
 
 # class Reply
 #     def self.find_by_id
