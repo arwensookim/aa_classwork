80d2e5dca1688349bd36d7ce55cc043b03fb7c08
arwen <arwenkim@Arwens-MBP.lan1>
2021-08-04 11:10:04 -0700

    create table in sql file

---
+++ w5d3/aa_project/import_db.sql
@@ -0,0 +1,22 @@
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS questions;
+DROP TABLE IF EXISTS question_follows;
+DROP TABLE IF EXISTS replies;
+DROP TABLE IF EXISTS question_likes;
+
+PRAGMA foreign_keys = ON;
+
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY,
+    fname TEXT NOT NULL,
+    lname TEXT NOT NULL
+);
+
+CREATE TABLE questions (
+    id INTEGER PRIMARY KEY,
+    title TEXT NOT NULL,
+    body TEST NOT NULL,
+    author_id INTEGER NOT NULL
+
+    FOREIGN KEY(author_id) REFERENCES users(id)
+);
