c353a407e9e9d11a9122e00d18ff89a677cfbc30
Alex Crooks <alexcrooks2@gmail.com>
2021-08-04 11:30:41 -0700

    replies/question follows/question likes v1 done

--- w5d3/aa_project/import_db.sql
+++ w5d3/aa_project/import_db.sql
@@ -15,8 +15,38 @@ CREATE TABLE users (
 CREATE TABLE questions (
     id INTEGER PRIMARY KEY,
     title TEXT NOT NULL,
-    body TEST NOT NULL,
-    author_id INTEGER NOT NULL
+    body TEXT NOT NULL,
+    author_id INTEGER NOT NULL,
 
     FOREIGN KEY(author_id) REFERENCES users(id)
+);
+
+CREATE TABLE question_follows (
+    id INTEGER PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    question_id INTEGER NOT NULL,
+
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    FOREIGN KEY(question_id) REFERENCES questions(id)
+);
+
+CREATE TABLE replies (
+    id INTEGER PRIMARY KEY,
+    question_id INTEGER NOT NULL,
+    parent_reply_id INTEGER,
+    user_id INTEGER NOT NULL,
+    body TEXT NOT NULL,
+
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    FOREIGN KEY(question_id) REFERENCES questions(id)
+    FOREIGN KEY(parent_reply_id) REFERENCES replies(id)
+);
+
+CREATE TABLE question_likes (
+    id INTEGER PRIMARY KEY
+    user_id INTEGER NOT NULL,
+    question_id INTEGER NOT NULL
+
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    FOREIGN KEY(question_id) REFERENCES questions(id)
 );
