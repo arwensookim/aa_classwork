4f9fd4c2d0a59459331edf142740ceb315d9444a
arwen <arwenkim@Arwens-MacBook-Pro.local>
2021-08-16 15:41:04 -0700

    create session_controller, working on application_controller

--- W7/w7d1/skeleton/app/controllers/application_controller.rb
+++ W7/w7d1/skeleton/app/controllers/application_controller.rb
@@ -1,2 +1,29 @@
 class ApplicationController < ActionController::Base
+    #  CRRLLL
+
+    def current_user
+        @current_user ||= User.finy_by(session_token: session[:session_token])
+    end
+
+    def require_logged_in
+        
+    end
+
+    def require_logged_out
+    end
+
+    def login(user)
+        session[:session_token] = user.reset_session_token!
+    end
+
+    def logged_in?
+        !!current_user
+    end
+
+    def logout!
+        current_user.reset_session_token! if logged_in?
+        session[:session_token] = nil
+        @current_user = nil
+    end
+
 end
