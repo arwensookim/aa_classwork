9115aeee9700bca8bce23e3d17904006d480a0e7
atjohnfeng <atjohnfeng@gmail.com>
2021-08-26 16:02:32 -0700

    Added addNumbers and absurdBubbleSort

--- W8/w8d4/IntroToCallBack/IntroCB.js
+++ W8/w8d4/IntroToCallBack/IntroCB.js
@@ -1,42 +1,87 @@
-class Clock {
-  constructor() {
-    let date = new Date();   
+// class Clock {
+//   constructor() {
+//     let date = new Date();   
 
-    this.hours = date.getHours();
-    this.minutes = date.getMinutes();
-    this.seconds = date.getSeconds();
+//     this.hours = date.getHours();
+//     this.minutes = date.getMinutes();
+//     this.seconds = date.getSeconds();
 
-    this.printTime();
+//     this.printTime();
+//     setInterval(this._tick.bind(this), 1000);
+//   };
 
-    setInterval(this._tick.bind(this),1000);
+//   printTime() {
+//     let time = `${this.hours}:${this.minutes}:${this.seconds}`;
+//     console.clear();
+//     console.log(time);
+//   };
 
-  };
+//   _tick() {
+//     this.seconds += 1;
+//     if (this.seconds === 60) {
+//       this.seconds = 0;
+//       this.minutes += 1;
+//       if (this.minutes === 60) {
+//         this.hours += 1;
+//         this.minutes = 0;
+//         if (this.hours === 24) {
+//           this.hours = 0;
+//         }
+//       }
+//     }
+//     this.printTime();
+//   };
+// }
 
-  printTime() {
-    console.log(this);
-    let time = `${this.hours}:${this.minutes}:${this.seconds}`;
-    console.log(time);
-    
-  };
+// let myClock = new Clock();
 
-  _tick() {
-    console.log("this is inside the tick function", this);
-    this.seconds += 1;
-    if (this.seconds === 60) {
-      this.seconds = 0;
-      this.minutes += 1;
-      if (this.minutes === 60) {
-        this.hours += 1;
-        this.minutes = 0;
-        if (this.hours === 24) {
-          this.hours = 0;
-        }
-      }
+// 
+
+const readline = require("readline");
+
+const reader = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+// function addNumbers(sum, numsLeft, completionCallback) {
+//     if (numsLeft > 0) {
+//         reader.question("Put a number: ", num => {
+//             let currentNum = parseInt(num);
+//             sum += currentNum;
+//             console.log(sum);
+//             addNumbers(sum, numsLeft-1, completionCallback);
+//         })
+//     } else {
+//         completionCallback(sum);
+//     }
+// }
+
+// addNumbers(0, 5, sum => console.log(`Total Sum: ${sum}`));
+
+// 
+
+function absurdBubbleSort(arr, sortCompletionCallback) {
+    function outerBubbleSortLoop(madeAnySwaps) {
+
     }
-    this.printTime();
-  };
+}
+
+function askIfGreaterThan(ele1, ele2, callback) {
+    reader.question(`Is ${ele1} greater than ${ele2}?`, response => {
+        if (response === 'Yes') {
+            callback(true);
+        } else {
+            callback(false);
+        }
+    })
+}
+
+function innerBubbleSortLoop(arr, i, madeAnySwaps, outerBubbleSortLoop) {
 
 }
 
-let myClock = new Clock();
-console.log(myClock);
+absurdBubbleSort([3, 2, 1], function (arr) {
+    console.log("Sorted array: " + JSON.stringify(arr));
+    reader.close();
+});
