3de4217cb9d4d43b2b92bda7efd4fd85e34e049c
Arwen <arwensookim@gmail.com>
2021-09-13 13:44:52 -0700

    W11 folder added and minesweeper folder added for project

---
+++ W11/w11d1/Minesweeper/react_minesweeper.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Game from "./components/game"
+
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementById("root");
+  const test = <h1>T</h1>;
+  ReactDOM.render(<Game /> ,root)
+})
